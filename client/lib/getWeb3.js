bec35e7115d2d5a92cbcd7906bf48a600feb254a
Jason Sprouse <jason.a.sprouse@gmail.com>
2020-12-22 04:05:23 -0600

    working application

--- client/lib/getWeb3.js
+++ client/lib/getWeb3.js
@@ -2,7 +2,7 @@ import Web3 from 'web3'
 
 const resolveWeb3 = (resolve) => {
   let { web3 } = window
-  // const alreadyInjected = typeof web3 !== 'undefined' // i.e. Mist/Metamask
+  const alreadyInjected = typeof web3 !== 'undefined' // i.e. Mist/Metamask
   const localProvider = `http://localhost:9545`
 
  
@@ -10,14 +10,14 @@ const resolveWeb3 = (resolve) => {
   const provider = new Web3.providers.HttpProvider(localProvider)
   web3 = new Web3(provider)
 
-  // if (alreadyInjected) {
-  //   console.log(`Injected web3 detected.`)
-  //   web3 = new Web3(web3.currentProvider)
-  // } else {
-  //   console.log(`No web3 instance injected, using Local web3.`)
-  //   const provider = new Web3.providers.HttpProvider(localProvider)
-  //   web3 = new Web3(provider)
-  // }
+  if (alreadyInjected) {
+    console.log(`Injected web3 detected.`)
+    web3 = new Web3(web3.currentProvider)
+  } else {
+    console.log(`No web3 instance injected, using Local web3.`)
+    const provider = new Web3.providers.HttpProvider(localProvider)
+    web3 = new Web3(provider)
+  }
 
   resolve(web3)
 }
