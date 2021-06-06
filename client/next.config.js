476b69a8c0cde351c662c1f135cb1b45aa71d538
Jason Sprouse <jason@goodfaith.church>
2021-06-06 04:44:50 -0500

    + headers & new dev chain for cors issues

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,13 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = {
   webpack: (config, { buildId, dev }) => {
     // This allows the app to refer to files through our symlink
     config.resolve.symlinks = false
     return config
-  }
+  },
+  async headers() {
+    return [{ source: "/(.*)", headers: createSecureHeaders() }];
+  },
+  
 }
--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 9545,
+      port: 8545,
       network_id: '*' // Match any network id
     },
     rinkeby: {
@@ -18,12 +18,4 @@ module.exports = {
         version: "^0.8.4"
       },
   }
-  
-    // Turns on the Solidity optimizer. For development the optimizer's
-    // quite helpful, just remember to be careful, and potentially turn it
-    // off, for live deployment and/or audit time. For more information,
-    // see the Truffle 4.0.0 release notes.
-    //
-    // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
-
 }
